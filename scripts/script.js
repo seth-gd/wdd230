426bc68dea114b6271781692c3b37e62a0c05551
seth-gd <u443447760@byui.edu>
2023-01-09 11:28:46 -0600

    Added missing semicolons

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // Display current year
-const d = new Date()
-document.querySelector('#current-year').textContent = d.getFullYear()
+const d = new Date();
+document.querySelector('#current-year').textContent = d.getFullYear();
 
 // Display Last Updated
 document.querySelector('#last-updated').textContent = document.lastModified;
