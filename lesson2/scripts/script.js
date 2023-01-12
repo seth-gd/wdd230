a0051c4eb01abe50e876c61b774966bd5be2585a
seth-gd <u443447760@byui.edu>
2023-01-12 17:50:49 -0600

    Added Design Principles Document

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,6 @@
+// Display current year
+const d = new Date();
+document.querySelector('#current-year').textContent = d.getFullYear();
+
+// Display Last Updated
+document.querySelector('#last-updated').textContent = document.lastModified;
