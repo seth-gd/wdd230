1f83f3d999475da269725fd6b0848869e2326c2a
seth-gd <u443447760@byui.edu>
2024-01-13 21:13:15 -0600

    Change last modified date format to UK

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,6 @@
+// Display current year
 document.querySelector('#year').textContent = new Date().getFullYear();
-document.querySelector('#last-modified').textContent = `Last modification: ${document.lastModified}`
+// Display last modified date in UK format
+const lmd = new Date(document.lastModified); // lmd: Last Modified Date
+const lastModifStr = `${lmd.getDate()}/${lmd.getMonth() + 1}/${lmd.getFullYear()} ${lmd.getHours()}:${lmd.getMinutes()}:${lmd.getSeconds()}`;
+document.querySelector('#last-modified').textContent = `Last modification: ${lastModifStr}`;
